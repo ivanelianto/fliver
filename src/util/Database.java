fc2ec736d43111eb81159692dfed18900411ec4d
Ivan Elv <purenaturalism@gmail.com>
2019-11-27 15:02:11 +0700

    Add Files InMemory

--- src/util/Database.java
+++ src/util/Database.java
@@ -2,14 +2,20 @@ package util;
 
 import java.util.ArrayList;
 
+import file.File;
+import file.HTMLFile;
 import model.User;
 
 public class Database
 {
 	public static ArrayList<User> users = new ArrayList<User>();
+	
+	public static ArrayList<File> files = new ArrayList<File>();
 
 	static
 	{
 		users.add(new User("iv", "something"));
+		
+		files.add(new HTMLFile());
 	}
 }
