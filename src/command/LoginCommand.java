e6d7aca671c4e44e82eeea25949bac12a37f88bd
Ivan Elv <purenaturalism@gmail.com>
2019-11-27 14:12:44 +0700

    Add Login and Open Explroer Command. Change StartupComand

--- src/command/LoginCommand.java
+++ src/command/LoginCommand.java
@@ -1,13 +1,35 @@
 package command;
 
+import model.User;
+import util.Database;
+
 public class LoginCommand implements Command
 {
+	private User user;
+	private String username;
+	private String password;
+
+	public LoginCommand(String username, String password)
+	{
+		this.username = username;
+		this.password = password;
+	}
 
 	@Override
 	public void execute()
 	{
-		clearScreen();
-		
+		this.user = Database.users.stream()
+			.filter(x -> 
+				x.getUsername().equals(this.username)
+				&& x.getPassword().equals(this.password)
+			)
+			.findAny()
+			.orElse(null);
+	}
+
+	public User getLoggedInUser()
+	{
+		return this.user;
 	}
 
 }
