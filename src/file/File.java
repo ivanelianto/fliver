e295ecdbf816bb4fca806322c2956112d9a6cc00
Ivan Elv <purenaturalism@gmail.com>
2019-11-27 15:02:25 +0700

    Refactoring File

--- src/file/File.java
+++ src/file/File.java
@@ -1,10 +1,37 @@
 package file;
 
-public interface File
+public abstract class File
 {
-	void run();
+	public static final int PERMISSION_INDEX = 0;
+	public static final int CREATE_TIME_INDEX = 1;
+	public static final int FILENAME_INDEX = 2;
+	
+	private String name;
+	
+	public abstract void run();
+	
+	public String getName() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("---------- ");
+		sb.append("00:00 ");
+		return sb.toString();
+	}
+	
+	public String getLastCharacter(int length)
+	{
+		int nameLength = this.getName().length();
+		
+		return this.getName().substring(
+				nameLength - length,
+				nameLength - 1);
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
-	Folder getFileContainer();
+	public abstract Folder getFileContainer();
 
-	void setFileContainer(Folder container);
+	public abstract void setFileContainer(Folder container);
 }
