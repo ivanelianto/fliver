4a410e1e9169ee9765d1d798a8eeed8894a7119f
Ivan Elv <purenaturalism@gmail.com>
2019-11-27 14:14:25 +0700

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
+import command.StartupCommand;
+import handler.ApplicationHandler;
+import handler.AuthenticationHandler;
+import handler.FileHandler;
 
 public class Main
 {
+
 	public Main()
 	{
-		
+		System.out.println("==========");
+		System.out.println("| Fliver |");
+		System.out.println("==========");
+
+		ApplicationHandler handler = new ApplicationHandler();
+		handler.setNext(new AuthenticationHandler()).setNext(new FileHandler());
+
+		while (true)
+			handler.process(new StartupCommand());
 	}
 
 	public static void main(String[] args)
