f601e408dc5e9fb0fd8fa61790cd5c74bfdb8ab6
Ivan Elv <purenaturalism@gmail.com>
2019-11-28 10:35:15 +0700

    Change Name Access Modifier

--- src/file/File.java
+++ src/file/File.java
@@ -6,29 +6,30 @@ public abstract class File
 	public static final int CREATE_TIME_INDEX = 1;
 	public static final int FILENAME_INDEX = 2;
 	
-	private String name;
+	protected String name;
 	
+	public File(String name)
+	{
+		this.name = name;
+	}
+
 	public abstract void run();
 	
 	public String getName() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("---------- ");
 		sb.append("00:00 ");
+		sb.append(name);
 		return sb.toString();
 	}
 	
 	public String getLastCharacter(int length)
 	{
-		int nameLength = this.getName().length();
+		int nameLength = this.name.length();
 		
-		return this.getName().substring(
+		return this.name.substring(
 				nameLength - length,
 				nameLength - 1);
-	}
-	
-	public void setName(String name)
-	{
-		this.name = name;
 	}
 
 	public abstract Folder getFileContainer();
--- src/file/HTMLFile.java
+++ src/file/HTMLFile.java
@@ -3,10 +3,15 @@ package file;
 public class HTMLFile extends PlainFile
 {
 
+	public HTMLFile(String name)
+	{
+		super(name);
+	}
+
 	@Override
 	public void run()
 	{
-		
+
 	}
 
 }
