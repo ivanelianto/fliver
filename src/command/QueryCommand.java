80e928cc61fac70e7ea8ef222ea0dcb39cad7783
Ivan Elv <purenaturalism@gmail.com>
2019-12-16 13:54:35 +0700

    Add query command.

--- src/command/QueryCommand.java
+++ src/command/QueryCommand.java
@@ -1,9 +1,16 @@
 package command;
 
-public abstract class QueryCommand implements Command
+import facade.FileFacade;
+import util.MyScanner;
+
+public class QueryCommand implements Command
 {
-	private String[] arguments;
+	private static final String DUMMY_USER_AND_HOST_NAME = "trainee@fliver";
+
+	private static final String EXIT_TEXT = "exit";
 	
+	private String[] arguments;
+
 	public String[] getArguments()
 	{
 		return this.arguments;
@@ -12,5 +19,30 @@ public abstract class QueryCommand implements Command
 	public void setArguments(String... arguments)
 	{
 		this.arguments = arguments;
+	}
+
+	@Override
+	public void execute()
+	{
+		do
+		{
+			String text = String.format("%s:%s$ ", 
+					DUMMY_USER_AND_HOST_NAME, 
+					FileFacade.getInstance()
+						.getCurrentFolder()
+						.getCompleteFilePath());
+			
+			System.out.print(text);
+			
+			String queryCommandInput = MyScanner.getString();
+
+			if (queryCommandInput.equalsIgnoreCase(EXIT_TEXT))
+				return;
+
+			CommandQueryBuilder builder = new CommandQueryBuilder(queryCommandInput);
+			QueryCommand queryCommand = builder.getCommand();
+			queryCommand.execute();
+		}
+		while (true);
 	}
 }
