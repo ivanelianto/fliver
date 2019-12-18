5063549e8d8044db8f2609e7a72f68decf21cea4
Ivan Elv <purenaturalism@gmail.com>
2019-12-18 18:30:50 +0700

    Implement make directory command execution.

--- src/command/MakeDirectoryCommand.java
+++ src/command/MakeDirectoryCommand.java
@@ -1,13 +1,39 @@
 package command;
 
+import facade.FileFacade;
+import file.Folder;
+
 public class MakeDirectoryCommand extends QueryCommand
 {
 
 	@Override
 	public void execute()
 	{
-		// TODO Auto-generated method stub
-		
+		if (!isValidArguments())
+			return;
+
+		for (String argument : this.getArguments())
+		{
+			FileFacade.getInstance()
+				.getCurrentFolder()
+				.addFile(new Folder(argument));
+		}
+	}
+
+	private boolean isValidArguments()
+	{
+		if (!isValidArgumentLength())
+		{
+			System.err.println("Too many arguments.");
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean isValidArgumentLength()
+	{
+		return this.getArguments().length > 0;
 	}
 
 }
