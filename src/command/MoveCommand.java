ad1551d14b7147d4f158bf5b21bbfd5774ee3562
Ivan Elv <purenaturalism@gmail.com>
2019-12-19 00:45:50 +0700

    Refactor move command.

--- src/command/MoveCommand.java
+++ src/command/MoveCommand.java
@@ -11,44 +11,21 @@ public class MoveCommand extends QueryCommand
 	private static final int SOURCE_INDEX = 0;
 	private static final int DESTINATION_INDEX = 1;
 
+	private String sourceFilePath;
+	private String destinationFilePath;
+	private File sourceFile;
+	private File destinationFile;
+
 	@Override
 	public void execute()
 	{
 		if (!isValidArguments())
 			return;
-		
-		String sourcePath = this.getArguments()[SOURCE_INDEX];
-		String destinationPath = this.getArguments()[DESTINATION_INDEX];
 
-		File sourceFile = getFile(sourcePath);
-		File destinationFile = getFile(destinationPath);
-		
-		if (!(sourceFile instanceof EmptyFile))
-		{
-			if (!(destinationFile instanceof EmptyFile))
-			{
-				if (!(destinationFile instanceof Folder))
-				{
-					System.err.println("The destination file should be directory.");
-				}
-				else
-				{
-					File copiedFile = sourceFile.copy();
-					((Folder) destinationFile).addFile(copiedFile);
-					
-					sourceFile.getParentFolder().deleteFile(sourceFile.getName());
-					System.out.println("File moved.");
-				}
-			}
-			else
-			{
-				System.err.println("Couldn't find the destination file specified.");
-			}
-		}
-		else
-		{
-			System.err.println("Couldn't find the source file specified.");
-		}
+		sourceFilePath = this.getArguments()[SOURCE_INDEX];
+		destinationFilePath = this.getArguments()[DESTINATION_INDEX];
+
+		processCommand();
 	}
 
 	@Override
@@ -78,11 +55,53 @@ public class MoveCommand extends QueryCommand
 		return this.getArguments().length < ARGUMENT_LENGTH;
 	}
 
+	private void processCommand()
+	{
+		if (processSourceFile() && processDestinationFile())
+		{
+			if (!(destinationFile instanceof Folder))
+				System.err.println("The destination file should be directory.");
+			else
+			{
+				File copiedFile = sourceFile.copy();
+				((Folder) destinationFile).addFile(copiedFile);
+
+				sourceFile.getParentFolder().deleteFile(sourceFile.getName());
+				System.out.println("File moved.");
+			}
+		}
+	}
+
+	private boolean processSourceFile()
+	{
+		sourceFile = getFile(sourceFilePath);
+
+		if (sourceFile instanceof EmptyFile)
+		{
+			System.err.println("Couldn't find the source file specified.");
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean processDestinationFile()
+	{
+		destinationFile = getFile(destinationFilePath);
+
+		if (destinationFile instanceof EmptyFile)
+		{
+			System.err.println("Couldn't find the destination file specified.");
+			return false;
+		}
+
+		return true;
+	}
+
 	private File getFile(String fileName)
 	{
-		Folder currentFolder = FileFacade.getInstance()
-				.getCurrentFolder();
-		
+		Folder currentFolder = FileFacade.getInstance().getCurrentFolder();
+
 		return currentFolder.getFiles().stream()
 				.filter(x -> x.getName().trim().equals(fileName))
 				.findAny()
