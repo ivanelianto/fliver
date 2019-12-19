1ee74385007a338b33324e98b769b24e288b9415
Ivan Elv <purenaturalism@gmail.com>
2019-12-19 13:24:18 +0700

    Validate create file arguments.

--- src/builder/FileBuilder.java
+++ src/builder/FileBuilder.java
@@ -12,62 +12,33 @@ public class FileBuilder extends BaseBuilder
 	{
 		this.setArguments(new ArrayList<String>(Arrays.asList(inputtedFiles)));
 	}
-	
+
 	@Override
 	public Object getResult()
 	{
 		ArrayList<PlainFile> files = new ArrayList<>();
+		
+		for (String argument : this.getArguments())
+		{
+			int dotIndex = argument.lastIndexOf('.');
+			
+			String extension = argument.substring(dotIndex + 1, argument.length());
 
-		if (!isValidArguments())
-		{
-			System.err.println("Missing arguments.");
-		}
-		else
-		{
-			for (String argument : this.getArguments())
+			try
 			{
-				int dotIndex = getDotIndex(argument);
-
-				if (dotIndex == -1)
-				{
-					System.err.println("Missing file extension.");
-					break;
-				}
-
-				String extension = argument.substring(dotIndex + 1, argument.length());
-
-				try
-				{
-					PlainFile file = Constants.fileTypes.get(extension).newInstance();
-					file.setName(argument);
-					files.add(file);
-				}
-				catch (Exception e)
-				{
-					e.printStackTrace();
-					
-					System.err.println(String.format("'%s' has invalid file extension.",
-							argument));
-					break;
-				}
+				PlainFile file = Constants.fileTypes.get(extension).newInstance();
+				file.setName(argument);
+				files.add(file);
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+				break;
 			}
 		}
 		
+		System.out.println(String.format("%d file(s) created.", files.size()));
+		
 		return files;
-	}
-
-	private boolean isValidArguments()
-	{
-		return hasValidArgumentLength();
-	}
-
-	private boolean hasValidArgumentLength()
-	{
-		return this.getArguments().size() > 0;
-	}
-
-	private int getDotIndex(String argument)
-	{
-		return argument.lastIndexOf('.');
 	}
 }
--- src/command/MakeFileCommand.java
+++ src/command/MakeFileCommand.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import builder.FileBuilder;
 import facade.FileFacade;
 import file.PlainFile;
+import util.Constants;
 
 public class MakeFileCommand extends QueryCommand
 {
@@ -14,6 +15,8 @@ public class MakeFileCommand extends QueryCommand
 	{
 		if (!isValidArguments())
 			return;
+		
+		normalizeArguments();
 		
 		FileBuilder builder = new FileBuilder(this.getArguments());
 		ArrayList<PlainFile> files = (ArrayList<PlainFile>) builder.getResult();
@@ -33,6 +36,11 @@ public class MakeFileCommand extends QueryCommand
 			System.err.println("Missing arguments.");
 			return false;
 		}
+		else if (!isValidFilePattern())
+		{
+			System.err.println("Missing file extension.");
+			return false;
+		}
 		
 		return true;
 	}
@@ -40,5 +48,37 @@ public class MakeFileCommand extends QueryCommand
 	private boolean isValidArgumentLength()
 	{
 		return this.getArguments().length > 0;
+	}
+	
+	private boolean isValidFilePattern()
+	{
+		for (String argument : this.getArguments())
+		{
+			int dotIndex = argument.lastIndexOf('.');
+
+			if (dotIndex == -1)
+				return false;
+		}
+		
+		return true;
+	}
+	
+	private void normalizeArguments()
+	{
+		ArrayList<String> validArguments = new ArrayList<>();
+		
+		for (String argument : this.getArguments())
+		{
+			int dotIndex = argument.lastIndexOf('.');
+			
+			String extension = argument.substring(dotIndex + 1, argument.length());
+			
+			if (Constants.fileTypes.get(extension) != null)
+				validArguments.add(argument);
+			else
+				System.err.println(String.format("%s has invalid extension.", argument));
+		}
+		
+		this.setArguments(validArguments.toArray(new String[]{}));
 	}
 }
