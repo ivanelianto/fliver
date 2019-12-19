d23f52bad922cfb7b10f6244eefb20012818c783
Ivan Elv <purenaturalism@gmail.com>
2019-12-19 12:27:25 +0700

    Add list command to validate not accept any arguments.

--- src/command/ListCommand.java
+++ src/command/ListCommand.java
@@ -4,13 +4,31 @@ import facade.FileFacade;
 
 public class ListCommand extends QueryCommand
 {
-
 	@Override
 	public void execute()
 	{
+		if (!isValidArguments())
+			return;
+		
 		FileFacade.getInstance()
 			.getCurrentFolder()
 			.retrieveAll();
 	}
 
+	@Override
+	public boolean isValidArguments()
+	{
+		if (!isValidArgumentLength())
+		{
+			System.err.println("Too many arguments.");
+			return false;
+		}
+		
+		return true;
+	}
+
+	private boolean isValidArgumentLength()
+	{
+		return this.getArguments().length == 0;
+	}
 }
